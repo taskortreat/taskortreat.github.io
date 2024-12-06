0cf4783f73f18e6914acaef997ed909fa2d8a65a
Douglas Land <looprock@users.noreply.github.com>
2024-12-06 08:57:30 -0600

    Update App.jsx
    Adding real donation link

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <p>Below is a generator for a note you can print out and give to whomever you want to do a Task or Treat for. The original ideas was that you could canvas your neighborhood, apartment building, etc. with notes and let people reach out to you.</p>
       <p>The benefits of giving are <a href="https://omny.fm/shows/a-slight-change-of-plans/get-happier-with-gretchen-rubin" target="_blank" rel="noopener noreferrer">well documented</a>, so do some good in the world and make yourself happier in the process.</p>
       <DocumentGenerator />
-      If you like what we're doing, please consider <a href="https://buymeacoffee.com/faq" target="_blank" rel="noopener noreferrer">donating</a>.
+      If you like what we're doing, please consider <a href="https://buymeacoffee.com/looprock" target="_blank" rel="noopener noreferrer">donating</a>.
     </div>
   );
 }
