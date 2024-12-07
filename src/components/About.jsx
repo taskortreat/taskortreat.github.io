f6943ded77342fee23b3e8710ff39b6d9569edf9
Douglas Land <dsl@looprock.com>
2024-12-07 00:24:05 -0600

    add About page

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div>
+      <h1>About Task or Treat</h1>
+      <p>Task or Treat is a community initiative that combines the spirit of Halloween with the joy of giving. Instead of asking for candy, participants offer to do tasks for their neighbors or provide small treats.</p>
+      <p>Join us in spreading kindness and community spirit!</p>
+    </div>
+  );
+}
+
+export default About;
